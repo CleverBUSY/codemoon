4e8afcfbda844650cad398718ce8f69208b06898
Sanjar <126055171+sanjar002@users.noreply.github.com>
2024-01-22 14:50:28 +0300

    Project-test

--- src/pages/Comands.jsx
+++ src/pages/Comands.jsx
@@ -146,10 +146,6 @@ const Comands = () => {
         </div>
 
         <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr 1fr",
-          }}
           className="comandsPhoto"
         >
           {teamData.map((member) => (
